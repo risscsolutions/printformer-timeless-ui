71d8a787b4d6a5eaba194779ec300ec07919d9e0
Gregor Kusmirek <gku@rissc.com>
2022-08-10 19:50:19 +0200

    New loader svg

--- src/components/FullScreenLoader.vue
+++ src/components/FullScreenLoader.vue
@@ -7,8 +7,8 @@
                  style="background: white; opacity:.5;"></div>
             <div class="column pl-0"
                  style="background: white;display: flex;flex-direction: column;align-content: center;justify-content: center;flex-wrap: wrap;align-items: center;">
-                <span style="  animation: loader-ring 1.2s cubic-bezier(0.5, 0, 0.5, 1) infinite;"
-                      v-html="$svg('VectorizerLadebalken')"></span>
+                <span style="animation: 1.6s cubic-bezier(0.46, 0.03, 0.52, 0.96) 0s infinite normal none running loader-ring;"
+                      v-html="$svg('Ladebalken','svg-150')"></span>
                 <p>Wird geladen...</p>
             </div>
             <div v-if="editorLoaded && !traceControlsIsOpen" class="column px-0 is-1 width-105" style="background: white; opacity:.5;"></div>
