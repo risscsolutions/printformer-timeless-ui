007f9364aae1e89b35a1014a00e9f063873865a3
lucap <luc@rissc.de>
2022-08-26 16:07:06 +0200

    Insert missing paragraph

--- src/components/sidebar/left/TraceControlInfo.vue
+++ src/components/sidebar/left/TraceControlInfo.vue
@@ -28,13 +28,14 @@
         </template>
         <template v-else-if="traceStep === 4">
             <p>{{ $translate('SIDEBAR_LEFT_VECTORIZER_STEP_4_PARAGRAPH_1') }}</p><br>
-            <p v-html="step4Paragraph2"></p><br>
-            <p>{{ $translate('SIDEBAR_LEFT_VECTORIZER_STEP_4_PARAGRAPH_3') }}</p><br>
-            <p v-html="step4Paragraph4"></p>
-            <p>{{ $translate('SIDEBAR_LEFT_VECTORIZER_STEP_4_PARAGRAPH_5') }}</p>
-            <p>{{ $translate('SIDEBAR_LEFT_VECTORIZER_STEP_4_PARAGRAPH_6') }}</p><br>
+            <p>{{ $translate('SIDEBAR_LEFT_VECTORIZER_STEP_4_PARAGRAPH_2') }}</p><br>
+            <p v-html="step4Paragraph3"></p><br>
+            <p>{{ $translate('SIDEBAR_LEFT_VECTORIZER_STEP_4_PARAGRAPH_4') }}</p><br>
+            <p v-html="step4Paragraph5"></p>
+            <p>{{ $translate('SIDEBAR_LEFT_VECTORIZER_STEP_4_PARAGRAPH_6') }}</p>
             <p>{{ $translate('SIDEBAR_LEFT_VECTORIZER_STEP_4_PARAGRAPH_7') }}</p><br>
-            <p>{{ $translate('SIDEBAR_LEFT_VECTORIZER_STEP_4_PARAGRAPH_8') }}</p>
+            <p>{{ $translate('SIDEBAR_LEFT_VECTORIZER_STEP_4_PARAGRAPH_8') }}</p><br>
+            <p>{{ $translate('SIDEBAR_LEFT_VECTORIZER_STEP_4_PARAGRAPH_9') }}</p>
         </template>
     </div>
 </template>
@@ -46,13 +47,13 @@ export default {
     name: "TraceControlInfo",
     computed: {
         ...mapState(['traceStep']),
-        step4Paragraph2() {
-            return this.$translate('SIDEBAR_LEFT_VECTORIZER_STEP_4_PARAGRAPH_2', {
+        step4Paragraph3() {
+            return this.$translate('SIDEBAR_LEFT_VECTORIZER_STEP_4_PARAGRAPH_3', {
                 symbol: `<button class="button is-rounded color-button-round m-0">${this.$svg('Plus')}</button>`
             })
         },
-        step4Paragraph4() {
-            return this.$translate('SIDEBAR_LEFT_VECTORIZER_STEP_4_PARAGRAPH_4', {
+        step4Paragraph5() {
+            return this.$translate('SIDEBAR_LEFT_VECTORIZER_STEP_4_PARAGRAPH_5', {
                 symbol: `<button class="button is-rounded color-button-round m-0">${this.$svg('Plus')}</button>`
             })
         }
